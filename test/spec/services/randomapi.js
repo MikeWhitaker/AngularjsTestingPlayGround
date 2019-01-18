939e851fc25cb98db4682a9c4a6b82e1a681e473
Michael Whitaker <Michael.Whitaker@figlo-nl.local>
2019-01-18 12:41:51 +0100

    added factory.

--- test/spec/services/randomapi.js
+++ test/spec/services/randomapi.js
@@ -1,18 +1,45 @@
-'use strict';
+"use strict";
 
-describe('Service: randomApi', function () {
+describe("Service: randomApi", function() {
+  var serviceData = {
+    title: "example glossary",
+    GlossDiv: {
+      title: "S",
+      GlossList: {
+        GlossEntry: {
+          ID: "SGML",
+          SortAs: "SGML",
+          GlossTerm: "Standard Generalized Markup Language",
+          Acronym: "SGML",
+          Abbrev: "ISO 8879:1986",
+          GlossDef: {
+            para:
+              "A meta-markup language, used to create markup languages such as DocBook.",
+            GlossSeeAlso: ["GML", "XML"]
+          },
+          GlossSee: "markup"
+        }
+      }
+    }
+  };
 
   // load the service's module
-  beforeEach(module('angularJsUnitTestingApp'));
+  beforeEach(module("angularJsUnitTestingApp"));
 
   // instantiate service
   var randomApi;
-  beforeEach(inject(function (_randomApi_) {
+  beforeEach(inject(function(_randomApi_) {
     randomApi = _randomApi_;
   }));
 
-  it('should do something', function () {
-    expect(!!randomApi).toBe(true);
+  it("should return some data", function() {
+    var service = {
+      getData: function() {
+        return serviceData.GlossDiv.GlossList.GlossEntry.GlossTerm;
+      }
+    };
+    expect(service.getData("something")).toEqual(
+      "Standard Generalized Markup Language"
+    );
   });
-
 });
