56574d65e1151295f07d6c77c6c5682f894ce2fc
DESKTOP-3SE1LP0\Mike <whitaker.mj@gmail.com>
2019-01-26 12:23:34 +0100

    changed some jslint and hint settings no not irritate me.

--- app/scripts/services/randomapi.js
+++ app/scripts/services/randomapi.js
@@ -1,36 +1,40 @@
-(function (){
+(function() {
+  "use strict";
 
-'use strict';
+  /**
+   * @ngdoc service
+   * @name angularJsUnitTestingApp.randomApi
+   * @description
+   * # randomApi
+   * Factory in the angularJsUnitTestingApp.
+   */
+  angular
+    .module("angularJsUnitTestingApp")
+    .factory("randomApi", randomApiFactory);
 
-/**
- * @ngdoc service
- * @name angularJsUnitTestingApp.randomApi
- * @description
- * # randomApi
- * Factory in the angularJsUnitTestingApp.
- */
-angular.module('angularJsUnitTestingApp')
-  .factory('randomApi', randomApiFactory);
-  
-  randomApiFactory.$inject = ['$http','$q'];
-  
-  function randomApiFactory($http,$q) {
+  randomApiFactory.$inject = ["$http", "$q"];
+
+  function randomApiFactory($http, $q) {
     // Service logic
     var service = {};
-    var baseUrl = 'http://omdbapi.com/?;' 
-  service.search = function (query) {
-    http.get(baseUrl + 's=' + encodeURIComponent(query))
-      .success(function(data){
-      });
-  };
+    var baseUrl = "http://omdbapi.com/?;";
+    service.search = function(query) {
+      var deferred = $q.defer();
+      $http
+        .get(baseUrl + "s=" + encodeURIComponent(query))
+        .success(function(data) {
+          deferred.resolve(data);
+        });
+        return deferred.promise;
+    };
 
-  service.find = function(searchString){
-    return {
-      Title: "StarWars",
-      Year: '1977'
-    }
-  };
+    service.find = function(searchString) {
+      return {
+        Title: "StarWars",
+        Year: "1977"
+      };
+    };
 
-  return service;
+    return service;
   }
-}());
+})();
--- test/spec/services/randomapi.tests.js
+++ test/spec/services/randomapi.tests.js
@@ -1,28 +1,37 @@
-'use strict';
+"use strict";
 
-fdescribe('Service: randomApi', function() {
+describe("Service: randomApi", function() {
   // load the service's module
   var randomApi = {};
-  beforeEach(module('angularJsUnitTestingApp'));
-  beforeEach(inject(function(_randomApi_){
+  beforeEach(module("angularJsUnitTestingApp"));
+  beforeEach(inject(function(_randomApi_) {
     randomApi = _randomApi_;
   }));
   // instantiate service
-  
-  fit('should return some data', function() {
-    // var someObject = {
-    //   foo: 'bar'
-    // };
-    
-    // dump(angular.mock.dump(someObject));
 
-    expect(randomApi.getData('something')).toEqual(
-      'Standard Generalized Markup Language'
-    );
+  fit("should return some data", function() {
+    // Arrange
+    var actualResponse;
+    // Act
+    randomApi.search("something")
+      .then(function(data){
+        actualResponse = data;
+      });
+
+    // Assert
+    var expectedResult = "example glossary";
+
+    expect().toEqual(expectedResult);
   });
 
-  it('should re turn the title', function() {
-    expect(randomApi.getTitle()).toEqual('example glossary');
-  });
+  describe("find method", function() {
+    it("should return a specific string", function() {
+      // Arrange
+      // Act
+      // Assert
+      var expectedResult = "example glossary";
 
+      expect(randomApi.find()).toEqual(expectedResult);
+    });
+  });
 });
